7a4b1f56316661d37b8f105db65c11c93236c62e
pramudya3 <pramudya500@gmail.com>
2023-01-16 20:57:09 +0700

    add get total saldo

--- main.go
+++ main.go
@@ -12,12 +12,21 @@ import (
 )
 
 const (
-	LoginURL = "https://ib.bri.co.id/login/"
+	LoginURL = "https://ib.bri.co.id/"
 )
 
-type Config struct {
-	Username string
-	Password string
+func getCaptcha(img []byte) string {
+	client := gosseract.NewClient()
+	err := client.SetImageFromBytes(img)
+	if err != nil {
+		log.Fatalln("error getting captcha.png ", err)
+	}
+	text, err := client.Text()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("captcha text: ", text)
+	return text
 }
 
 func main() {
@@ -25,7 +34,7 @@ func main() {
 	defer browser.MustClose()
 
 	page := stealth.MustPage(browser)
-	page.MustNavigate("https://ib.bri.co.id/").MustWindowNormal()
+	page.MustNavigate(LoginURL).MustWindowNormal()
 
 	// get captcha text
 	client := gosseract.NewClient()
@@ -35,22 +44,28 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// client.SetImage("captcha.png")
-	err = client.SetImageFromBytes(img)
-	if err != nil {
-		log.Fatalln("error getting captcha.png ", err)
-	}
-	text, err := client.Text()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("captcha text: ", text)
+	text := getCaptcha(img)
 
 	// isi form login
 	page.MustElement("#loginForm > div.validation > input[type=text]").MustInput(text)
 	page.MustElement("#loginForm > input[type=text]:nth-child(5)").MustInput("")
 	page.MustElement("#loginForm > input[type=password]:nth-child(8)").MustInput("")
-	page.MustElement("#loginForm > button").MustClick()
+	page.MustElement("#loginForm > button").MustClick().GetSessionID()
+
+	// get total rekening
+	page.MustElement("#myaccounts > table").MustClick()
+	page.MustElement("body > div.submenu.active > div:nth-child(2) > a").MustClick()
+	noRek := page.MustElement("#Any_0 > td:nth-child(1)")
+	jenisProduk := page.MustElement("#Any_0 > td:nth-child(2)")
+	nama := page.MustElement("#Any_0 > td:nth-child(3)")
+	mataUang := page.MustElement("#Any_0 > td:nth-child(4)")
+	saldo := page.MustElement("#Any_0 > td:nth-child(5)")
+
+	fmt.Println("Nomor Rekening : ", noRek.MustText())
+	fmt.Println("Jeni Produk : ", jenisProduk.MustText())
+	fmt.Println("Nama : ", nama.MustText())
+	fmt.Println("Mata Uang : ", mataUang.MustText())
+	fmt.Println("Saldo : ", saldo.MustText())
 
 	time.Sleep(time.Hour)
 }
