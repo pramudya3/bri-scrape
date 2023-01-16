198f7416adafe17794e609a9e525562cce6e05fd
pramudya3 <pramudya500@gmail.com>
2023-01-16 07:51:57 +0700

    login bri with capctha solver

---
+++ go.mod
@@ -0,0 +1,15 @@
+module bri
+
+go 1.19
+
+require (
+	github.com/go-rod/rod v0.112.3
+	github.com/otiai10/gosseract/v2 v2.4.0
+)
+
+require (
+	github.com/go-rod/stealth v0.4.8 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/stealth"
+	"github.com/otiai10/gosseract/v2"
+)
+
+const (
+	LoginURL = "https://ib.bri.co.id/login/"
+)
+
+type Config struct {
+	Username string
+	Password string
+}
+
+func main() {
+	browser := rod.New().Timeout(time.Minute).MustConnect()
+	defer browser.MustClose()
+
+	page := stealth.MustPage(browser)
+	// page := browser.MustPage("https://ib.bri.co.id/ib-bri/login/").MustWindowNormal()
+	page.MustNavigate("https://ib.bri.co.id/")
+
+	// get captcha text
+	client := gosseract.NewClient()
+	defer client.Close()
+
+	page.MustElement("#simple_img > img").MustWaitLoad().MustScreenshot("captcha.png")
+	text, err := client.Text()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("captcha text: ", text)
+
+	// isi form login
+	page.MustElement("#loginForm > div.validation > input[type=text]").MustInput(text)
+	page.MustElement("#loginForm > input[type=text]:nth-child(5)").MustInput("rizkypramudya3")
+	page.MustElement("#loginForm > input[type=password]:nth-child(8)").MustInput("Pramudya3")
+	page.MustElement("#loginForm > button").MustClick()
+
+	time.Sleep(time.Hour)
+}
